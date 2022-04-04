ca0115ce2d790e24a6fe94d31933b82fef00698c
ppavelk <ppavelk@mail.ru>
2022-04-04 12:50:51 +0300

    добавил проверку страницы регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -15,3 +16,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+   
+    pagel = LoginPage(browser, link)
+    pagel.open()
+    pagel.should_be_login_page()
